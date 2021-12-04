7843e48191ddbd84fab51c135a04b695bcab89c3
tobiashjalmarsson <tobhja@student.chalmers.se>
2021-12-04 02:14:07 +0100

    Frontend fileupload

--- frontend/src/components/UploadComponent/UploadComponent.tsx
+++ frontend/src/components/UploadComponent/UploadComponent.tsx
@@ -2,7 +2,7 @@ import {FC, useState} from 'react'
 import './styles.scss'
 
 const UploadComponent: FC = () => {
-
+    const [passkey, setPasskey] = useState("")
     const [selectedFile, setSelectedFile] = useState({
         name: "",
         size: 0,
@@ -20,6 +20,21 @@ const UploadComponent: FC = () => {
         setIsFilePicked(true)
     }
 
+    const updatePasskey = (e : any) => {
+        e.preventDefault()
+        setPasskey(e.target.value)
+    }
+
+    const removeFile = (e : any) => {
+        e.preventDefault()
+        setSelectedFile({
+            name: "",
+            size: 0,
+            type: "" 
+        })
+        setIsFilePicked(false)
+    }
+
     return (
         <div className="mainContainer">
             <h2>Upload</h2>
@@ -28,9 +43,11 @@ const UploadComponent: FC = () => {
                 <p>Name: {selectedFile.name}</p>
                 <p>Type: {selectedFile.type}</p>
                 <p>Size (Bytes): {selectedFile.size}</p> 
+                <button onClick={removeFile}>Remove</button>
             </div>
             }
-            <input type="file" name="file" onChange={handleFileChange}/>
+            <input id="fileInput" type="file" name="file" onChange={handleFileChange}/>
+            <input id="passkeyInput" type="text" name="passkey" onChange={updatePasskey} value={passkey} placeholder="Passkey"/>
             <button onClick={handleSubmit}>Upload</button> 
         </div>
     )
